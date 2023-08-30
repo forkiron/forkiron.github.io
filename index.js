90246315808f8a62e019c2d47ecb90c30a54a3f6
Thomas <thomaslenh@gmail.com>
2023-08-30 02:11:54 -0400

    did underlines for about tabs

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ function whenScrollAbout() {
         }
         else{
             currenttitle.style.opacity = '0'
+            currenttitle.style.transition = 'opacity 1s ease-out'
         }
 
     });
@@ -129,23 +130,43 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 // when the tab title is clicked, bring in whatever tab is clicked
-function openTab(tabthatisclicked, tabtitle, tabcolor){
+function openTab(tabthatisclicked, tabtitle, tabcolor, tabunderline){
     //Assign whatever tab is clicked to this constant to be used to set visible
     const tabshow = document.getElementById(tabthatisclicked);
     const tabtitleshow = document.getElementById(tabtitle);
+    const tabunderlineshow = document.getElementById(tabunderline);
 
     //First, hide all other tabs
-    hideothertabs(tabthatisclicked, tabtitle);
+    hideothertabs(tabthatisclicked, tabtitle, tabunderlineshow);
     //Show the tab that is clicked
+
     tabtitleshow.style.textShadow = `0px 0px 20px ${tabcolor}`;
+    tabunderlineshow.style.opacity = '0.7'
+
+    tabunderlineshow.style.animation = 'none'
+    tabunderlineshow.offsetWidth; // Trigger a reflow to reset and recalculate
+
+    if (tabunderline === 'aboutUnderline') {
+        tabunderlineshow.style.animation = 'aboutUnderlineIn 0.5s ';
+    } else if (tabunderline === 'hobbiesUnderline') {
+        tabunderlineshow.style.animation = 'hobbiesUnderlineIn 0.5s ';
+    } else if (tabunderline === 'skillsUnderline') {
+        tabunderlineshow.style.animation = 'skillsUnderlineIn 0.5s ';
+
+    }
+    
+
+    // Add the animation class to the tabunderline element
+    //classlist add the underline
     tabshow.style.visibility = 'visible';
 
 }
 //hides all tabs except the one that is clicked, hence bringing in 'tabthatisclicked'
-function hideothertabs(donotinclude, donotincludetitle){
+function hideothertabs(donotinclude, donotincludetitle, donotincludeunderline){
     //Make a list with all the tabs that I have
     const tabs = ['aboutword', 'skillsword', 'hobbiesword']
     const tabtitles = ['aboutwordtitle', 'skillswordtitle', 'hobbieswordtitle']
+    const tabunderlines = ['aboutUnderline', 'hobbiesUnderline', 'skillsUnderline']
     // for each loop (basically for-in) that goes through the list of tabs that I have 
     tabs.forEach(tab => {
         //if the tab in the list happens to NOT be equal to 'tabthatisclicked', proceed. This excludes 'tabthatisclicked' from being hidden
@@ -168,7 +189,56 @@ function hideothertabs(donotinclude, donotincludetitle){
 
         }
     });
-        
+    tabunderlines.forEach(underline => {
+        if (underline !== donotincludeunderline){
+            const underlineselected = document.getElementById(underline)
+            underlineselected.style.opacity = '0'
+        }
+    })
         
     
 }
+
+//placed outside because used in 2 functions
+const meunderline = document.getElementById('meUnderline')
+//Listen to when content is loaded then is a function that controlls the underline under 'me'
+document.addEventListener("DOMContentLoaded", function (){
+    
+    const subheadingme = document.getElementById('subheadingme')
+    
+
+    //Listens to when the mouse enters the 'me' word
+    subheadingme.addEventListener('mouseenter', function(){
+        //When it does, make the underline appear, and also stretch out
+        meunderline.classList.add('visibleUnderline')
+    });
+    //Listens to when the mouse leaves the 'me' word
+    subheadingme.addEventListener('mouseleave', function(){
+        //Function to find the current width of the underline
+        checkUnderlineWidth();
+        //Add another class which retracts the underline back to opacity 0
+        meunderline.classList.add('invisibleUnderline');
+        //Remove the other classlist which would otherwise prevent 'invisibleunderline' to retract due to css rules overlaying
+        meunderline.classList.remove('visibleUnderline');
+    });
+       
+
+});
+
+//function that checks the width of the underline
+function checkUnderlineWidth(){
+    //.clientwidth finds width of an object
+    const width = meunderline.clientWidth;
+    console.log(width);
+    //Create an animation rule that replicates 'invisibleunderline', just with the current width of the underline
+    const animationRule = `@keyframes meunderlineback { 0% { width: ${width}px; opacity: 1; } 100% { width: 0; opacity: 0; } }`;
+    //Locate the style sheet using .stylesheets
+    const styleSheet = document.styleSheets[1];
+    //In the stylesheet, delete the current rules, and more specifically, the width value of 'meunderlineback' to add the new/current width value
+    styleSheet.deleteRule('meunderlineback');
+    //Insert the rule, (which is why this keyframe rule is at the very top (ik this isn't very organized and efficient work))
+    styleSheet.insertRule(animationRule);
+}
+
+
+
